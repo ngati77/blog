f23a2535322f8ed90e3dcca4ec6b2e25d8bfad32
owner <owner@owners-MacBook-Pro.local>
2020-01-05 22:48:00 +0000

    added blog_linebreak_url filter

---
+++ templatetags/blog_extras.py
@@ -0,0 +1,34 @@
+import re
+from django import template
+from django.template.defaultfilters import stringfilter
+
+from django.template.defaultfilters import linebreaksbr, urlize, safe
+
+# @register.filter(needs_autoescape=True)
+# def urlize_and_linebreaks(text, autoescape=True):
+#     return linebreaksbr(
+#         urlize(text, autoescape=autoescape),
+#         autoescape=autoescape
+#     )
+
+
+register = template.Library()
+
+@register.filter
+@stringfilter
+def BlogCreateLink(text): # Only one argument.
+    """Converts a string contains [[<url link>,<text>]] to hyperlink"""
+    # The pattern search for [[ and ]] in between it seperate according to <,> into two groups,
+    # The first part is the link while the second is text to display
+    pattern = '\[\[(.*?),(.*?)\]\]'
+    replacement = r'<a target=”_blank” href=\1>\2</a>'
+    result = re.sub(pattern, replacement, text)
+    return result
+
+@register.filter(needs_autoescape=True)
+def blog_url_and_linebreaks(text, autoescape=True):
+    """ This functionwrap the linebreak and the new url, then call the safe function"""
+    return safe(BlogCreateLink(
+        linebreaksbr(text,
+        autoescape=autoescape))
+    )
